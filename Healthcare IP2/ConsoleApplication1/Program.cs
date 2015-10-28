44f536c8ea3d56a32f030407b44de9f0702cb16e
Yorick Rommers <yorick-rommers@hotmail.com>
2015-10-28 23:07:28 +0100

    Added server

--- Healthcare IP2/ConsoleApplication1/Program.cs
+++ Healthcare IP2/ConsoleApplication1/Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
     class Server
     {
-        
+        private TcpListener server;
+        private List<ClientHandler> clients;
 
         static void Main(string[] args)
         {
@@ -17,7 +24,137 @@ namespace ConsoleApplication1
 
         public Server()
         {
+            this.clients = new List<ClientHandler>();
+
+
             Console.WriteLine("Starting server");
+            server = new TcpListener(IPAddress.Any, 8888);
+            server.Start();
+            BackgroundWorker bgw = new BackgroundWorker();
+            bgw.DoWork += (o, args) =>
+            {
+                while (true)
+                {
+                    clients.Add(new ClientHandler(server.AcceptTcpClient()));
+                }
+
+            };
+            bgw.RunWorkerAsync();
+            Console.ReadLine();
+        }
+    }
+
+    class ClientHandler
+    {
+        private StreamReader r;
+        private StreamWriter w;
+        private TcpClient client;
+
+        BackgroundWorker bgw = new BackgroundWorker();
+
+        public ClientHandler(TcpClient client)
+        {
+            this.client = client;
+            this.r = new StreamReader(client.GetStream());
+            this.w = new StreamWriter(client.GetStream());
+            bgw.DoWork += (o, args) =>
+            {
+                while (true)
+                {
+                    string text = r.ReadLine();
+                    if (text != null)
+                    {
+                        string[] values = text.Split('|');
+                        string command = values[0];
+                        switch (command)
+                        {
+                            case "get":
+                                Console.WriteLine("Getting data: "+ text);
+                                List<String> stringList = GetClientData(values[1]);
+                                if (stringList.Count > 0)
+                                {
+                                    stringList.ForEach(s =>
+                                    {
+                                        w.WriteLine(s);
+                                        w.Flush();
+                                    });
+                                }
+                                else
+                                {
+                                    w.WriteLine("ER|No files available.");
+                                    w.Flush();
+                                }
+                                
+                                break;
+                            case "set":
+                                Console.WriteLine("Setting data: "+ text);
+                                bool written = SetClientData(values[1],values[2]);
+                                if (written)
+                                {
+                                    w.WriteLine("OK|Write succesful.");
+                                    w.Flush();
+                                }
+                                else
+                                {
+                                    w.WriteLine("ER|Write unsuccesful.");
+                                    w.Flush();
+                                }
+                                break;
+
+                        }
+
+                        w.WriteLine(command);
+                        w.Flush();
+                    }
+                }
+            };
+            bgw.RunWorkerAsync();
+        }
+
+        private bool SetClientData(string clientname, string data)
+        {
+            bool done;
+            List<String> existingData = GetClientData(clientname);
+
+            try
+            {
+                using (StreamWriter stw = new StreamWriter(Environment.CurrentDirectory + "\\" + clientname + ".txt"))
+                {
+                    existingData.ForEach(s =>
+                    {
+                        stw.WriteLine(s);
+                    });
+                    stw.WriteLine(data);
+                    stw.Flush();
+                    done = true;
+                }
+            }
+            catch (Exception) {
+                done = false;
+            }
+            return done;
+
+        }
+
+        private List<string> GetClientData(string clientname)
+        {
+            List<String> stringList = new List<string>();
+            try {
+                using (StreamReader str = new StreamReader(Environment.CurrentDirectory + "\\" + clientname + ".txt"))
+                {
+                    string readLine;
+                    while ((readLine = str.ReadLine()) != null)
+                    {
+                        stringList.Add(readLine);
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                
+            }
+            
+            return stringList;
         }
     }
 }
---
+++ Healthcare IP2/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private StreamReader r;
+        private StreamWriter w;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new TcpClient("localhost",8888);
+            r = new StreamReader(client.GetStream());
+            w = new StreamWriter(client.GetStream());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
