1cd76322275a32306dce46e8bce9a5d458752076
yorickr <yorick-rommers@hotmail.com>
2015-10-28 21:30:42 +0100

    server

---
+++ Healthcare IP2/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Server
+    {
+        
+
+        static void Main(string[] args)
+        {
+            new Server();
+        }
+
+        public Server()
+        {
+            Console.WriteLine("Starting server");
+        }
+    }
+}
